c2f6815c470e896ad6a0f110d44751b626ed2823
Amir Fahimi <amir.fahimi.aviation@gmail.com>
2023-06-05 22:26:16 +0330

    add math.js

---
+++ jest-tutorial/math.js
@@ -0,0 +1,15 @@
+export const sum = (x, y) => {
+    return x + y
+}
+
+export const multiplication = (x, y) => {
+    return x * y;
+}
+
+export const subtraction = (x, y) => {
+    return x - y
+}
+
+export const division = (x, y) => {
+    return x / y
+}
